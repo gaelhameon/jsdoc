8bdca3201a4f343734afe3498424e8e2b86e2c5b
Gaël Haméon <gael.hameon@gmail.com>
2020-12-13 23:18:29 +0100

    feature(jsdoc): make standalone doclet

---
+++ packages/jsdoc/index.js
@@ -0,0 +1,36 @@
+
+const fs = require('fs');
+const path = require('path');
+const { config } = require('@jsdoc/core');
+
+let env;
+let jsdocPath = __dirname;
+
+// Create a custom require method that adds `lib/jsdoc` and `node_modules` to the module
+// lookup path. This makes it possible to `require('jsdoc/foo')` from external templates and
+// plugins, and within JSDoc itself. It also allows external templates and plugins to
+// require JSDoc's module dependencies without installing them locally.
+/* eslint-disable no-global-assign, no-redeclare */
+require = require('requizzle')({
+    requirePaths: {
+        before: [path.join(__dirname, 'lib')],
+        after: [path.join(__dirname, 'node_modules')]
+    },
+    infect: true
+});
+/* eslint-enable no-global-assign, no-redeclare */
+
+// resolve the path if it's a symlink
+if (fs.statSync(jsdocPath).isSymbolicLink()) {
+    jsdocPath = path.resolve(path.dirname(jsdocPath), fs.readlinkSync(jsdocPath));
+}
+
+env = require('./lib/jsdoc/env');
+env.dirname = jsdocPath;
+env.args = process.argv.slice(2);
+
+env.conf = config.loadSync(env.opts.configure).config;
+
+const { Doclet } = require('./lib/jsdoc/doclet');
+
+module.exports = { Doclet };
---
+++ packages/jsdoc/test/specs/jsdoc/standaloneDoclet.js
@@ -0,0 +1,45 @@
+const { Doclet } = require('../../../index');
+
+const myComment = `/**
+* @typedef FactorizeVscsInVscCollectionExecuteArgs
+* @property {VehicleSchedulesCollection} sourceVscColl
+* @property {Object|string} [configSet=toto]
+* @property {Object} other
+* @property {Number=} other.param
+*/`;
+
+const expectedResult = {
+    'comment': '/**\n * @typedef FactorizeVscsInVscCollectionExecuteArgs\n * @property {VehicleSchedulesCollection} sourceVscColl\n * @property {Object|string} [configSet=toto]\n * @property {Object} other\n * @property {Number=} other.param\n */',
+    'meta': { 'code': {} },
+    'kind': 'typedef',
+    'name': 'FactorizeVscsInVscCollectionExecuteArgs',
+    'properties': [{
+        'type': { 'names': ['VehicleSchedulesCollection'] },
+        'name': 'sourceVscColl'
+    }, {
+        'type': { 'names': ['Object', 'string'] },
+        'optional': true,
+        'defaultvalue': 'toto',
+        'name': 'configSet'
+    }, {
+        'type': { 'names': ['Object'] },
+        'name': 'other'
+    }, {
+        'type': { 'names': ['Number'] },
+        'optional': true,
+        'name': 'other.param'
+    }],
+    'longname': 'FactorizeVscsInVscCollectionExecuteArgs'
+};
+
+describe('standaloneDoclet', () => {
+    it('works', () => {
+        const myDoclet = new Doclet(myComment);
+
+        console.log(myDoclet);
+
+        expect(JSON.stringify(new Doclet(myComment))).toEqual(JSON.stringify(expectedResult));
+    });
+});
+
+
